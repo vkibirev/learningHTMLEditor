41cb845763e3e365441011e8783c38539ef7d734
kibirev (Виктор Кибирев) <kibirev@office.local>
2018-07-23 19:43:38 +0300

    several updates

--- src/index.js
+++ src/index.js
@@ -6,16 +6,20 @@ const createTags = require('./add');
             'div', '', [
             createTags.tag('div', '', [
                 createTags.tag('ul', '', [                    
-                    createTags.tag('li', 'hello'),                    
-                    createTags.tag('li', 'world'),
+                    createTags.tag('li', 'hello', []),                    
+                    createTags.tag('li', 'world', []),
                 ])
-            ], {class: 'block-left'}),
+            ], {class: 'block-left schema'}),
             createTags.tag('div', '', [
-                createTags.tag('ul', '', [                    
-                    createTags.tag('li', 'hello'),                    
-                    createTags.tag('li', 'world'),
-                ])
-            ], {class: 'block-right'})
+                createTags.tag('div', '', [
+                    createTags.tag('div', '', [
+                        createTags.tag('ul', '', [                    
+                            createTags.tag('li', 'hello'),                    
+                            createTags.tag('li', 'world'),
+                        ]),
+                    ])
+                ], {class: 'board'}),                
+            ], {class: 'block-right'}),
         ], {class: 'row'}), 
         createTags.tag('div', '', [
             createTags.tag('div', '', [
@@ -26,8 +30,35 @@ const createTags = require('./add');
 
             ], {class: 'block-left'}),
             createTags.tag('div', '', [
-                createTags.tag('div', 'Current tag: ')
+                createTags.tag('div', `Current tag: `, [], {id: "currentTag"})
             ], {class: 'block-right'})
         ], {class: 'row'}),
     ]),
     document.querySelector('script'));
+
+let board = document.querySelector('.board');
+board.addEventListener('click', handler);
+    
+function handler(event) {   
+    let current = document.querySelector('.active'); 
+
+    if (current) {
+        document.querySelector('#currentTag').textContent = `Current tag: ` + current.tagName.toLowerCase();        
+        current.classList.remove('active');  
+    }    
+    event.target.classList.toggle('active');   
+
+    // if (current) {        
+    // document.querySelector('#currentTag').textContent = `Current tag: ` + current.tagName.toLowerCase();       
+    // }  
+}
+
+// let  = 
+// function tree() {
+
+    
+// }
+
+
+
+
