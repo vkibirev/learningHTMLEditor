9513e3aff751db19865622d49ee1e37cba90b9f9
unknown <vitya.kibirev@gmail.com>
2018-07-25 12:06:42 +0300

    set text, add tag and remove features realised

--- src/index.js
+++ src/index.js
@@ -37,34 +37,106 @@ const createTags = require('./add');
     document.querySelector('script'));
 
 let board = document.querySelector('.board');
-board.addEventListener('click', handler);
-    
-function handler(event) {   
-    let current = document.querySelector('.active'); 
-    
-    if (current) {        
-        current.classList.remove('active');  
-    }   
-    
-    event.target.classList.toggle('active');    
+board.addEventListener('click', clickOnBoardItem());
 
-    activeTag(); 
+function activeExists(){
+    let active = document.querySelector('.active');
+    if(active){
+        return active;
+    }
+    return false;
+}
+    
+function clickOnBoardItem(event) {
+    let tempActiveTag;
+
+    return function userClick(event) {
+        let currentActiveTag = event.target;
+
+        if (currentActiveTag !== tempActiveTag && activeExists()) {
+            activeExists().classList.remove('active');
+        }
+        event.target.classList.toggle('active');
+        showCurrentTag(event.target);
+        tempActiveTag = event.target;
+    }
 }
 
-function activeTag() {    
-    document.querySelector('#currentTag').textContent = `Current tag: ` + document.querySelector('.active').tagName.toLowerCase();
+function showCurrentTag(target) {
+    let activeTagName = activeExists().tagName.toLowerCase();
+    let currentTagField =  document.querySelector('#currentTag');
+    
+    if (activeExists()) {
+        return currentTagField.textContent = `Current tag: ${activeTagName}`;
+    }
+    return;    
 }
 
-let tags = ['h1', 'h2', 'p', 'ul', 'li'];
+
 
 let set = document.querySelector('.setter');
 set.addEventListener('click', setText);
+
 function setText() {
-    let current = document.querySelector('.active');
-    if (current) {        
-        current.textContent = input.value;  
+    if (activeExists() && input.value === '') {  
+        return activeExists().textContent = "default"; 
+    }
+    return activeExists().textContent = input.value;
+    
+}
+
+let createTag = document.querySelector('.createTag');
+createTag.addEventListener('click', addTag);
+
+function addTag() {
+    
+    let tags = ['h1', 'h2', 'h3', 'h4', 'div', 'p', 'ul', 'ol', 'li'];
+    if (activeExists() && !tags.includes(input.value)) {     
+        alert(`${input.value} value aren't allowed you can use only these tags: h1, h2, h3, h4, div, p, ul, ol, li`);                   
+        return;
+    }
+    return activeExists().appendChild(createTags.tag(input.value, ''));
+}
+
+let remove = document.querySelector('.remove');
+remove.addEventListener('click', removeTag);
+
+function removeTag() {
+    if (activeExists()) {
+        return activeExists().remove();
     }
 }
+
+
+
+// let set = document.querySelector('.setter');
+// let createTag = document.querySelector('.createTag');
+// let current = document.querySelector('.active');
+// set.addEventListener('click', adder(setText, current));
+// createTag.addEventListener('click', adder(createTag, current));
+
+
+// function adder(func, cur) {
+    
+//     console.log(cur);
+    
+//     if (cur) {        
+//         return func;
+//     }
+// }
+
+// function setText() {
+//     console.log('testesteste');
+// }
+
+// function createTagFunc() {
+//     console.log('create tag');
+    
+// }
+
+
+
+
 
 
 
